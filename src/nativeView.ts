e4f737b9fbf2ff3a1e60dc56bc1167a50b82f2d1
pengxp <pengxp@by-health.com>
2018-10-29 10:41:04 +0800

    Add native view enum

---
+++ src/nativeView.ts
@@ -0,0 +1,10 @@
+/*
+ * BY-Health Front-end Team (https://www.by-health.com/)
+ *
+ * Copyright © 2016-present By-Health Co Ltd. All rights reserved.
+ */
+
+export const FindPassword = {
+  Android: 'com.by_health.memberapp.activity.serviceplus.FindPasswordActivity',
+  iOS: 'ExGetPasswordStep1ViewController',
+};
